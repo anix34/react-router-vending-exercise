d1d28b0b4dab7869bbf010f9a81fdd373e48760f
anix34 <139282344+anix34@users.noreply.github.com>
2024-06-30 18:48:36 -0400

    Add files via upload

---
+++ my-project/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import VendingMachine from './VendingMachine';
+import Snack1 from './Snack1';
+import Snack2 from './Snack2';
+import Snack3 from './Snack3';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Route exact path="/snack1">
+          <Snack1 />
+        </Route>
+        <Route exact path="/snack2">
+          <Snack2 />
+        </Route>
+        <Route exact path="/snack3">
+          <Snack3 />
+        </Route>
+        <Route exact path="/">
+          <VendingMachine />
+        </Route>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
